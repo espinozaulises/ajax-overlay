f5b8f3c6f0ef501eb3a89ddbfca6bbdcfdfe2568
espinozaulises <espinoza.ulises@gmail.com>
2012-06-14 15:18:24 -0500

    Reescribiendo el código. Javascript orientado a Objetos.

--- js/jquery.ajaxOverlay.js
+++ js/jquery.ajaxOverlay.js
@@ -1,38 +1,83 @@
-$.fn.ajaxOverlay = function(options) {
-	var opts = $.extend({}, $.fn.ajaxOverlay.defaults, options); 
+;(function($, window, undefined) {
+    //$.fn.ajaxOverlay = function(options) {
 
-	if ( opts.action === 'start' )  {
-		$(this).data('position', $(this).css('position'));
-        $(this).data('overflow', $(this).css('overflow'));
-        $(this).css('position', 'relative');
-        $(this).css('overflow', 'hidden');
+        var Overlay = function(elem, opciones) {
 
-        // Propiedades del wrapper que contendrá los elementos del overlay
-        $('<div class="ui-overlay"></div>').css({
-            top: 0 + $(this).scrollTop(),
-            left: 0 + $(this).scrollLeft(),
-            width: $(this).outerWidth(),
-            height: $(this).outerHeight()
-        }).appendTo($(this));
+            this.elem  = elem;
+            this.$elem = $(elem);
 
-        // agrego la capa que contiene la animación y el mensaje
-        $(".ui-overlay").append('<div class="ui-block-message"><div class="ui-gif-loading"></div></div>');
+            if(this.init) {
+                this.init(opciones);
+            }
+        }
 
-        // Si hay un mensaje lo agregamos
-        if( opts.message != '' )
-            $(".ui-block-message").append('<span class="ui-text-loading">'+ opts.message +'</span>');
-    
-    // Si la acción solicitada es "end" removemos los divs
-    }   else if( opts.action === 'end') {
-        $(this).children('.ui-overlay').fadeOut(1000, function() {
-            $(this).remove();
-        });
-        
-    }
-};
+        Overlay.prototype = {
+            // Contiene los elementos de la plataforma.
+            defaults : {
+                message: 'Cargando...',
+                fadeout: 500,
+            },
+            init: function(opciones) {
 
-$.fn.ajaxOverlay.defaults = {
-	action: 'start',
-    message: 'Cargando...',
+                this.config = $.extend({}, this.defaults, opciones);
+                this.$contenedor = this.$elem;
+                this.$contenedor.css('position', 'relative');
+                this.$div_over = $('<div class="ui-overlay"></div>'); // Contenedor principal del overlay
+                this.$div_message = $('<div class="ui-block-message"><div class="ui-gif-loading"></div></div>');
+                this.$span_text = $('<span class="ui-text-loading"></span>');
+                this.scrolltop = this.$contenedor.scrollTop();
+                this.scrollLeft = this.$contenedor.scrollLeft();
+                this.oWidth = this.$contenedor.outerWidth();
+                this.oHeight = this.$contenedor.outerHeight();
 
-};
+                this.viewOver();
+            },
+            viewOver: function() {
+                this.$div_over.css({
+                    top: 0 + this.scrolltop,
+                    left: 0 + this.scrollLeft,
+                    width: this.oWidth,
+                    height: this.oHeight,
+                }).appendTo(this.$contenedor);
+
+                // agrego la capa que contiene la animación y el mensaje
+                this.$div_over.append(this.$div_message);
+                // Si hay un mensaje lo agregamos
+                if( this.config.message != '' ) {
+                    this.$span_text.html( this.config.message );
+                    this.$div_message.append( this.$span_text );
+                }
+            },
+
+            hidden: function(opciones) {
+               this.$div_over.fadeOut(this.config.fadeout, function() {
+                    this.remove();
+                });
+            },
+        }
+
+        $.fn.ajaxOverlay = function(opciones) {
+            if(typeof opciones == "string") {
+                metodo = opciones;
+                args = Array.prototype.slice.call(arguments, 1);
+
+                var overlay = this.data('overlay') ? 
+                    this.data('overlay') :
+                    new Overlay(this);
+
+                if( overlay[metodo] ) {
+                    overlay[metodo].apply(overlay, args);
+                }
+
+            } else if(typeof opciones == 'objet' || !opciones) {
+                this.data('overlay', new Overlay(this, opciones));
+            } else {
+                $.error("Error: Parámetros incorrectos.");
+            }
+
+            return this;
+        }
+
+        window.Overlay = Overlay;
+
+ })(jQuery, window);
